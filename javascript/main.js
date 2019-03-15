cf698ef42112835cf4b47552d26caf461de5d4a2
NicoP93 <nicopcrr@gmail.com>
2019-03-15 17:42:47 -0600

    Not so bad all in all.

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   var AnimalsArray = ["Dog", "Cat", "Hamster", "Lion"];
 
-//////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //for loop to make a new button with data-animal attribute 
 
   function renderButtons() {
@@ -28,7 +28,7 @@ $(document).ready(function () {
     event.preventDefault(); //dont refresh the page
     // This line grabs the input from the textbox
     var UserInput = $("#Animal-input").val().trim(); //take the input
-
+    
     // Adding Animal from the textbox to our array
     AnimalsArray.push(UserInput); //add it to the list
 
@@ -42,10 +42,11 @@ $(document).ready(function () {
 
 ////////////////////////////////////////////////////////////////////////////
 //ajax queryurl with our animal butttons and for loops to populate page from the button 
-  $("button").on("click", function () {
+  $("#buttons-view").on("click", "button", function () {
+    event.preventDefault();
     // In this case, the "this" keyword refers to the button that was clicked
     var animal = $(this).attr("data-animal");
-
+    
     // Constructing a URL to search Giphy for the name of the animal who said the quote
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -56,9 +57,10 @@ $(document).ready(function () {
       })
 
       .then(function (response) {
-
+      
         var results = response.data;
-
+        
+        
 
         for (var i = 0; i < results.length; i++) {
           if (results[i].rating !== "r") {
@@ -71,13 +73,24 @@ $(document).ready(function () {
             animalImage.attr("data-animated", results[i].images.fixed_height.url);
             animalImage.attr("data-still", results[i].images.fixed_height_still.url);
             animalImage.attr("src", results[i].images.fixed_height.url);
+////////////////////////////////////////////////////////////// :_( 
+
+
+
+            var rating = response.data[i].rating;  
+            var pOne = $("<p>").text("Rating: " + rating);
+          // Displaying the rating
+            gifDiv.append(pOne);
+
+          
+
           }//The gifts will pause but wont unpause 
         }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //on click function to pause and unpause, we can puase the images but it wont unpause 
-        debugger;
+     
         $(".gif").on("click", function() {
           // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
           var state = $(this).attr("data-state");
@@ -85,8 +98,8 @@ $(document).ready(function () {
           // Then, set the image's data-state to animate
           // Else set src to the data-still value
           if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animated");
           } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
